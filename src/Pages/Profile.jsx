e1e3eeaf19bf1be126860e2eb133a5bfbb27941a
davidtav <david.mclaurel@outlook.com.br>
2024-09-22 16:23:54 -0300

    implementação do componente Profile com a Lib ImagePicker

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,10 +1,64 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, View, Image, TextInput, Pressable } from "react-native";
+import { useEffect, useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+import Button from "../Components/Button/Button";
 
 export default function Profile() {
+  const [image, setImage] = useState(null);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    if (!image) {
+      pickImage();
+    }
+  }, [image]);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
-    <View>
-      <Text>Profile</Text>
+    <View className="p-3 flex-1">
+      {/* Avatar image Picker */}
+      {image ? (
+        <Image
+          source={{ uri: image }}
+          className="w-52 aspect-square self-center rounded-full bg-slate-300"
+        />
+      ) : (
+        <View className="w-52 aspect-square self-center rounded-full bg-slate-300" />
+      )}
+      <Text
+        onPress={pickImage}
+        className="text-blue-500 font-semibold m-5 self-center"
+      >
+        Change
+      </Text>
+      {/* Form */}
+      <Text className="mb-2 text-gray-500 font-semibold">Username</Text>
+      <TextInput
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+        className="border border-gray-300 p-3 rounded-md"
+      />
+      {/* Button */}
+      <View className="gap-2 mt-auto">
+        <Button nome="Update Profile" />
+        <Button nome="Sign Out" />
+      </View>
     </View>
-  )
+  );
 }
