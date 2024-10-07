a880b01b893aab7a0d7c2d34352bd01cd9f108ef
davidtav <david.mclaurel@outlook.com.br>
2024-10-06 23:49:28 -0300

    atualização de dependencias

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,8 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const { getDefaultConfig } = require('@expo/metro-config');
 
-module.exports = withNativeWind(config, { input: './global.css' })
+const defaultConfig = getDefaultConfig(__dirname);
+defaultConfig.resolver.sourceExts.push('cjs');
+
+module.exports = defaultConfig;
